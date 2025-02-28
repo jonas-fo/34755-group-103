52c4c38658e70985aeb4b0234a73b39abac7daee
Rasmus <raxypony@gmail.com>
2025-02-28 11:43:31 +0100

    added rightside follow fucntion

--- main.py
+++ main.py
@@ -177,3 +177,11 @@ def main():
     loop()
     service.terminate()
   print("% Main Terminated")
+
+def RightSide_follow(speed,time):
+  while (pose.tripBtimePassed() < time or edge.lineValidCnt == 0):
+    edge.lineControl(speed, -1.0) # keeps the line in the middle at  'speed'cm/s
+    #-1 might have to be changed to +1 depending on what wide of the line sensor is + and -
+    pass
+  edge.lineControl(0, 0)
+  pass
